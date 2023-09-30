b601a18c96291bb9bf344bf0e869d6b228b4fd45
anoop <anoopviswanadh007@gmail.com>
2023-09-30 18:22:11 +0530

    header text and btn alignments

--- src/widgets/heroBanner/heroBannerComponent.tsx
+++ src/widgets/heroBanner/heroBannerComponent.tsx
@@ -3,56 +3,63 @@ import HeroBanner from "@/interfaces/heroBanner";
 import Link from "next/link";
 
 export default function HeroBannerComponent({ heading, buttons, background }: HeroBanner) {
-    console.log(buttons);
+    console.log(`justify-content-${heading.position}`);
 
     return <div
         className={style.herobanner_outer}
         style={
-            { backgroundImage: `url(${background.lg})` }
+            {
+                backgroundImage: `url(${background.lg})`,
+            }
         }
     >
-        <h1 className={`d-none d-md-block ${style.hero_banner_heading}`}
-            dangerouslySetInnerHTML={{ __html: heading.label }}
-        />
-        <h1 className={`d-block d-md-none ${style.hero_banner_heading_md}`}
-            dangerouslySetInnerHTML={{ __html: heading.label }}
-        />
-        <br />
-        <br className="d-none d-md-flex" />
-        <div className="d-none d-md-flex">
-            {
-                buttons?.map(button => <Link
-                    href={button.url}>
-                    <button
-                        className={`
+        <div className="container">
+            <h1 className={`d-none d-md-block ${style.hero_banner_heading}`}
+                style={{
+                    textAlign: `${heading.position  || "center" }`
+                }}
+                dangerouslySetInnerHTML={{ __html: heading.label }}
+            />
+            <h1 className={`d-block d-md-none ${style.hero_banner_heading_md}`}
+                dangerouslySetInnerHTML={{ __html: heading.label }}
+            />
+            <br />
+            <br className="d-none d-md-flex" />
+            <div className={`d-none d-md-flex justify-content-${heading.position}`}>
+                {
+                    buttons?.map(button => <Link
+                        href={button.url}>
+                        <button
+                            className={`
                         ${button.boxShadow && style.box_shadow}
                         ${style.hero_banner_btn}
                         ${button.textshadow && style.text_shadow}
                         `}
-                        style={{
-                            color: `${button.color}`,
-                            backgroundColor: `${button.background}`
-                        }}
-                    >{button.label}</button>
-                </Link>)
-            }
-        </div>
-        <div className="d-flex d-md-none">
-            {
-                buttons?.map(button => <Link
-                    href={button.url}>
-                    <button className={`
+                            style={{
+                                color: `${button.color}`,
+                                backgroundColor: `${button.background}`
+                            }}
+                        >{button.label}</button>
+                    </Link>)
+                }
+            </div>
+            <div className="d-flex d-md-none">
+                {
+                    buttons?.map(button => <Link
+                        href={button.url}>
+                        <button className={`
                     ${button.boxShadow && style.box_shadow} 
                     ${style.hero_banner_btn_md}
                     ${button.textshadow && style.text_shadow}
                     `}
-                        style={{
-                            color: `${button.color}`,
-                            backgroundColor: `${button.background}`
-                        }}
-                    >View Products</button>
-                </Link>)
-            }
+                            style={{
+                                color: `${button.color}`,
+                                backgroundColor: `${button.background}`
+                            }}
+                        >View Products</button>
+                    </Link>)
+                }
+            </div>
         </div>
     </div>
 }
